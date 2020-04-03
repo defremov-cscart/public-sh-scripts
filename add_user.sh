79369b20df945cf3ae5e1e4e86641f8d4a7a928a
Dmitry Efremov <59552138+defremov-cscart@users.noreply.github.com>
2020-04-03 12:54:51 +0400

    Create add_user.sh

---
+++ add_user.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+### Written by Dmitry Efremov https://github.com/defremov-cscart
+echo -n "Enter username (e.g. iivanov) "
+
+read -p 'Username: ' adduser
+
+echo -n "Enter destination server address "
+
+read -p 'Destination server IP or domain name: ' destserver
+
+echo -n "Enter user's id_rsa.pub key "
+
+read -p 'Key: ' sshkey
+
+
+echo -n "Check your data. User - $adduser, destination server - $destserver. Continue? (Y/n) "
+read item
+  case "$item" in
+    y|Y) echo "Make some magic..." ;;
+    n|N) echo "Exiting..." exit 0 ;;
+    *) echo "Default choise is Yes, make some magic......"
+    ;;
+esac
+
+ssh $USER@$destserver << EOF
+sudo useradd -m -s /bin/bash ${adduser}
+sudo mkdir /home/$adduser/.ssh
+sudo sh -c "echo '$sshkey' >> /home/$adduser/.ssh/authorized_keys"
+sudo chown -R $adduser: /home/$adduser/
+sudo chmod -R  700 /home/$adduser/.ssh
+sudo sh -c "echo '$adduser      ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/admin"
+EOF
