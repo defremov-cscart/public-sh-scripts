bb984b6643f6a8d7acb217664756a06df3185d8b
defremov <defremov@cs-cart.com>
2021-03-18 15:16:39 +0400

    remote access remove

--- add_user.sh
+++ add_user.sh
@@ -4,16 +4,12 @@ echo -n "Enter username (e.g. iivanov) "
 
 read -p 'Username: ' adduser
 
-echo -n "Enter destination server address "
-
-read -p 'Destination server IP or domain name: ' destserver
-
 echo -n "Enter user's id_rsa.pub key "
 
 read -p 'Key: ' sshkey
 
 
-echo -n "Check your data. User - $adduser, destination server - $destserver. Continue? (Y/n) "
+echo -n "Create user with $adduser name? (Y/n) "
 read item
   case "$item" in
     y|Y) echo "Make some magic..." ;;
@@ -22,11 +18,9 @@ read item
     ;;
 esac
 
-ssh $USER@$destserver << EOF
-sudo useradd -m -s /bin/bash ${adduser}
+sudo useradd -m -s /bin/bash $adduser
 sudo mkdir /home/$adduser/.ssh
 sudo sh -c "echo '$sshkey' >> /home/$adduser/.ssh/authorized_keys"
 sudo chown -R $adduser: /home/$adduser/
 sudo chmod -R  700 /home/$adduser/.ssh
-sudo sh -c "echo '$adduser      ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/admin"
-EOF
+sudo usermod -aG docker $adduser
